612f400b7300f9a17db06e852d814c0d95b2d994
Brent Douglas <brent.n.douglas@gmail.com>
2015-08-16 10:17:50 +0200

    Add license headers

--- bench/mem/src/main/java/io/machinecode/vial/bench/mem/Memory.java
+++ bench/mem/src/main/java/io/machinecode/vial/bench/mem/Memory.java
@@ -1,19 +1,26 @@
+/*
+ * Copyright 2015 Brent Douglas and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.machinecode.vial.bench.mem;
 
 import com.google.caliper.memory.ObjectGraphMeasurer;
+import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.info.GraphLayout;
 
 import java.lang.instrument.Instrumentation;
-import java.lang.reflect.Array;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.Collections;
-import java.util.IdentityHashMap;
-import java.util.Set;
-import java.util.Stack;
 
 /**
- * From http://www.javaspecialists.eu/archive/Issue142.html
- *
  * @author <a href="mailto:brent.n.douglas@gmail.com">Brent Douglas</a>
  * @since 1.0
  */
@@ -30,92 +37,21 @@ public class Memory {
     }
 
     public static void printSizeOf(final String name, final Object that) {
-        System.out.println(name + "\t\t" + that.getClass().getSimpleName() + "\t\tsize: " + Memory.deepSizeOf(that));
-        System.out.println("\t\t" + ObjectGraphMeasurer.measure(that));
-    }
-
-    public static long sizeOf(final Object that) {
         assert instrumentation != null;
-        return instrumentation.getObjectSize(that);
-    }
-
-    public static long deepSizeOf(Object obj) {
-        Set<Object> visited = Collections.newSetFromMap(new IdentityHashMap<Object,Boolean>());
-        Stack<Object> stack = new Stack<>();
-        stack.push(obj);
-
-        long result = 0;
-        do {
-            result += internalSizeOf(stack.pop(), stack, visited);
-        } while (!stack.isEmpty());
-        return result;
-    }
-
-    private static boolean isSharedFlyweight(final Object obj) {
-        if (obj instanceof Comparable) {
-            if (obj instanceof Enum) {
-                return true;
-            } else if (obj instanceof String) {
-                return (obj == ((String) obj).intern());
-            } else if (obj instanceof Boolean) {
-                return (obj == Boolean.TRUE || obj == Boolean.FALSE);
-            } else if (obj instanceof Integer) {
-                return (obj == Integer.valueOf((Integer) obj));
-            } else if (obj instanceof Short) {
-                return (obj == Short.valueOf((Short) obj));
-            } else if (obj instanceof Byte) {
-                return (obj == Byte.valueOf((Byte) obj));
-            } else if (obj instanceof Long) {
-                return (obj == Long.valueOf((Long) obj));
-            } else if (obj instanceof Character) {
-                return (obj == Character.valueOf((Character) obj));
-            }
-        }
-        return false;
-    }
-
-    private static boolean skipObject(Object obj, Set<Object> visited) {
-        return obj == null
-                || visited.contains(obj)
-                || isSharedFlyweight(obj);
-    }
-
-    private static long internalSizeOf(final Object obj, final Stack<Object> stack, final Set<Object> visited) {
-        if (skipObject(obj, visited)) {
-            return 0;
-        }
-
-        Class clazz = obj.getClass();
-        if (clazz.isArray()) {
-            addArrayElementsToStack(clazz, obj, stack);
-        } else {
-            // add all non-primitive fields to the stack
-            while (clazz != null) {
-                Field[] fields = clazz.getDeclaredFields();
-                for (Field field : fields) {
-                    if (!Modifier.isStatic(field.getModifiers())
-                            && !field.getType().isPrimitive()) {
-                        field.setAccessible(true);
-                        try {
-                            stack.add(field.get(obj));
-                        } catch (final IllegalAccessException ex) {
-                            throw new RuntimeException(ex);
-                        }
-                    }
-                }
-                clazz = clazz.getSuperclass();
-            }
-        }
-        visited.add(obj);
-        return sizeOf(obj);
-    }
-
-    private static void addArrayElementsToStack(final Class<?> clazz, final Object obj, final Stack<Object> stack) {
-        if (!clazz.getComponentType().isPrimitive()) {
-            int length = Array.getLength(obj);
-            for (int i = 0; i < length; i++) {
-                stack.add(Array.get(obj, i));
-            }
-        }
+        System.out.println(name + " " + that.getClass().getSimpleName());
+        System.out.println("Instrumentation");
+        System.out.println("size: " + instrumentation.getObjectSize(that));
+        System.out.println("Caliper ObjectGraphMeasurer");
+        System.out.println(ObjectGraphMeasurer.measure(that));
+        System.out.println("JOL GraphLayout");
+        final GraphLayout gl = GraphLayout.parseInstance(that.getClass());
+        System.out.println(gl.toFootprint());
+        System.out.println(gl.toPrintable());
+        System.out.println(gl.totalSize());
+        System.out.println("JOL ClassLayout");
+        final ClassLayout cl = ClassLayout.parseClass(that.getClass());
+        System.out.println(cl.toPrintable(that));
+        System.out.println(cl.instanceSize());
+        System.out.println(cl.headerSize());
     }
 }
--- bench/mem/src/test/java/io/machinecode/vial/bench/mem/map/OOHashMapMemoryTest.java
+++ bench/mem/src/test/java/io/machinecode/vial/bench/mem/map/OOHashMapMemoryTest.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2015 Brent Douglas and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.machinecode.vial.bench.mem.map;
 
 import com.carrotsearch.hppc.ObjectObjectMap;
@@ -22,7 +36,7 @@ import java.util.Random;
 public class OOHashMapMemoryTest {
 
     private static final float FACTOR = 0.75f;
-    private static final int CAPACITY = 1000000;
+    private static final int CAPACITY = 1000;
     private static final int SEED = 0x654265;
 
     @Test
